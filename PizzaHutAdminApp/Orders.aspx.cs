24335d5957401ee9d1c79d862e9e2f266312cd0c
Ткаченко Владимир <tkachvova.94@gmail.com>
2015-04-13 00:26:08 +0300

    First Commit

---
+++ PizzaHutAdminApp/Orders.aspx.cs
@@ -0,0 +1,36 @@
+﻿using PizzaHutAdminApp.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PizzaHutAdminApp
+{
+    public partial class Orders : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void OrdersGridView1RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName != "ChangeOrderStatus") return;
+            int id = Convert.ToInt32(e.CommandArgument);
+
+            var repo = new PizzaHutDbEntities();
+            var order = repo.orders.FirstOrDefault(p => p.id == id);
+            if (order.processing == true)
+            {
+                order.processing = false;
+            }
+            else
+            {
+                order.processing = true;
+            }
+            repo.SaveChanges();
+            Response.Redirect(Request.RawUrl); 
+        }
+    }
+}
